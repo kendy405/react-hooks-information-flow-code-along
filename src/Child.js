d3181c4dfeb33a39fc38d3b6c61de97680ab93a0
Irene <irene.muthomi@student.moringaschool.com>
2023-11-06 21:42:59 +0300

    completed

--- src/Child.js
+++ src/Child.js
@@ -1,7 +1,28 @@
 import React from "react";
 import { getRandomColor } from "./randomColorGenerator.js";
 
-function Child({ onChangeColor }) {
+//function Child({ onChangeColor }) {
+function Child({ onChangeColor, color}) {
+  // console.log(onChangeColor);
+  function handleClick() {
+    const newColor = getRandomColor();
+    onChangeColor(newColor);
+  }
+  return (
+    <div
+      onClick={handleClick}
+      className="child"
+      style={{ backgroundColor: color}}
+    />
+  );
+  console.log(onChangeColor);
+return (
+  <div
+    onClick={onChangeColor}
+    className="child"
+    style={{ backgroundColor: color}}
+  />
+);
   return <div className="child" style={{ backgroundColor: "#FFF" }} />;
 }
 
